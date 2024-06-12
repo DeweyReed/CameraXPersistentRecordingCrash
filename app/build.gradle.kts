027322e357dd83d37364d5ded045610a6bf1495c
DeweyReed <32358486+DeweyReed@users.noreply.github.com>
2024-06-12 20:34:09 +0800

    Reproduce the problem

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -40,4 +43,21 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // CameraX core library using the camera2 implementation
+    val camerax_version = "1.4.0-beta01"
+    // val camerax_version = "1.4.0-alpha05"
+    // The following line is optional, as the core library is included indirectly by camera-camera2
+    // implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    // If you want to additionally use the CameraX Lifecycle library
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    // If you want to additionally use the CameraX VideoCapture library
+    implementation("androidx.camera:camera-video:${camerax_version}")
+    // If you want to additionally use the CameraX View class
+    implementation("androidx.camera:camera-view:${camerax_version}")
+    // If you want to additionally add CameraX ML Kit Vision Integration
+    // implementation("androidx.camera:camera-mlkit-vision:${camerax_version}")
+    // If you want to additionally use the CameraX Extensions library
+    // implementation("androidx.camera:camera-extensions:${camerax_version}")
 }
--- app/src/main/java/com/github/deweyreed/camerax/prc/MainActivity.kt
+++ app/src/main/java/com/github/deweyreed/camerax/prc/MainActivity.kt
@@ -1,20 +1,135 @@
 package com.github.deweyreed.camerax.prc
 
+import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.MirrorMode
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.FileOutputOptions
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
+import androidx.camera.video.VideoRecordEvent
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.github.deweyreed.camerax.prc.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var facingFront = false
+    private lateinit var preview: Preview
+    private lateinit var videoCapture: VideoCapture<Recorder>
+    private var recording: Recording? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (!it) {
+                finish()
+                return@registerForActivityResult
+            }
+            startCamera()
+        }.launch(Manifest.permission.CAMERA)
+
+        binding.btnFlip.setOnClickListener {
+            facingFront = !facingFront
+            startCamera()
+        }
+
+        binding.btnRecord.setOnClickListener {
+            if (recording == null) {
+                @Suppress("MissingPermission")
+                recording = videoCapture.output
+                    .prepareRecording(
+                        this,
+                        FileOutputOptions.Builder(
+                            File(filesDir, "video.mp4")
+                        ).build()
+                    )
+                    .asPersistentRecording()
+                    .start(
+                        ContextCompat.getMainExecutor(this)
+                    ) { event ->
+                        when (event) {
+                            is VideoRecordEvent.Start -> {
+                                binding.btnRecord.text = "Stop"
+                            }
+                            is VideoRecordEvent.Finalize -> {
+                                if (event.hasError()) {
+                                    Toast.makeText(this, event.error.toString(), Toast.LENGTH_SHORT)
+                                        .show()
+                                } else {
+                                    binding.btnRecord.text = "Start"
+                                }
+                            }
+                        }
+                    }
+            } else {
+                recording?.run {
+                    stop()
+                    recording = null
+                }
+            }
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture =
+            ProcessCameraProvider.getInstance(applicationContext)
+        cameraProviderFuture.addListener(
+            {
+                recording?.pause()
+
+                val cameraProvider = cameraProviderFuture.get()
+                cameraProvider.unbindAll()
+
+                val cameraSelector = CameraSelector.Builder()
+                    .requireLensFacing(
+                        if (facingFront) {
+                            CameraSelector.LENS_FACING_FRONT
+                        } else {
+                            CameraSelector.LENS_FACING_BACK
+                        }
+                    )
+                    .build()
+
+                if (!::preview.isInitialized) {
+                    preview = Preview.Builder().build()
+                    preview.setSurfaceProvider(binding.previewView.surfaceProvider)
+                }
+
+                if (!::videoCapture.isInitialized) {
+                    videoCapture = VideoCapture.Builder(Recorder.Builder().build())
+                        .setMirrorMode(MirrorMode.MIRROR_MODE_ON_FRONT_ONLY)
+                        .build()
+                }
+
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    videoCapture,
+                )
+
+                recording?.resume()
+            },
+            ContextCompat.getMainExecutor(this)
+        )
     }
 }
